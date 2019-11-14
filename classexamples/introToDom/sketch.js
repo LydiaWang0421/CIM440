be24e17f5262e4399f58aa4ece3965c25d8b34aa
LydiaWangTY <lydiawangty@Lydias-MacBook.local>
2019-11-14 11:52:44 -0500

    1114

--- classexamples/introToDom/sketch.js
+++ classexamples/introToDom/sketch.js
@@ -21,7 +21,6 @@ function setup() {
   select('body').style("background-color", "orange");
   var container1 = createDiv();
   container1.html('<iframe width="560" height="315" src="https://www.youtube.com/embed/yPWkPOfnGsw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"');
-
 }
 
 function draw() {
---
+++ hw/final/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  // put setup code here
+  createCanvas(1000,600);
+
+  
+} //end of setup
+
+function draw() {
+  // put drawing code here
+  background("PapayaWhip");
+
+
+} //end of draw
