5a4fab1fa33321d01fb1d04bcbe8fec62e3d53fe
LydiaWang0421 <54292759+LydiaWang0421@users.noreply.github.com>
2019-12-04 17:49:21 -0500

    1204

--- hw/final/sketch.js
+++ hw/final/sketch.js
@@ -20,14 +20,19 @@ var balloonY = [];
 var balloonAmount = 30;
 var balloonImage;
 
+var swingShow = false;
+var slideShow = false;
 var chessShow = false;
+var ropeShow = false;
+var seesawShow = false;
+
+var slideVid;
 var chessVid;
 
-var ropeShow = false;
-var asterisk;
-var platform;
-var GRAVITY = 1;
-var JUMP = 15;
+var birdX = 265;
+var birdY = 105;
+var birdW = 20;
+var birdH = 20;
 
 function preload(){
   parkImage = loadImage("images/park.png");
@@ -40,6 +45,7 @@ function preload(){
   balloonImage = loadImage("images/balloon.png");
 
   chessVid = createVideo("images/chessvid.mp4");
+  slideVid = createVideo("images/slidevid.mp4");
 
 } //end of preload
 
@@ -109,7 +115,11 @@ function setup() {
   this.textColor = "black";
   }
   swingClick.onPress = function(){
-    alert("Kids should always sit in the swing, not stand or kneel. They should hold on tightly with both hands while swinging, and when finished swinging, stop the swing completely before getting off. Children should stay a safe distance from other kids on swings, being careful not to run or walk in front of or in back of moving swings. Kids should never ride with more than one child to a swing. Swings are designed to safely hold only one person. ");
+    if(swingShow == true){
+      swingShow = false;
+    }else{
+      swingShow = true;
+    }
   }
 
   slideClick = new Clickable();
@@ -126,7 +136,12 @@ function setup() {
   this.textColor = "black";
   }
   slideClick.onPress = function(){
-    alert("Kids should always slide down feet first and sitting up, never head first or on their back or stomach. Only one child should be on the slide platform at a time, and kids shouldn't slide down in groups. Kids should always check that the bottom of the slide is clear before sliding down. ");
+    if(slideShow == true){
+      slideShow = false;
+      slideVid.hide();
+    }else{
+      slideShow = true;
+    }
   }
 
   chessClick = new Clickable();
@@ -170,13 +185,6 @@ function setup() {
     }else{
       ropeShow = true;
     }
-    asterisk = createSprite(865,100);
-    asterisk.addAnimation('normal', 'play/examples/assets/asterisk_normal0001.png', 'play/examples/assets/asterisk_normal0003.png');
-    asterisk.addAnimation('stretch', 'play/examples/assets/asterisk_stretching0001.png', 'play/examples/assets/asterisk_stretching0008.png');
-    asterisk.setCollider('circle', 0, 0, 64);
-
-    platform = createSprite(865,330);
-    platform.addAnimation('normal', 'play/examples/assets/small_platform0001.png', 'play/examples/assets/small_platform0003.png');
   }
 
   seesawClick = new Clickable();
@@ -193,11 +201,15 @@ function setup() {
   this.textColor = "black";
   }
   seesawClick.onPress = function(){
-    alert("Seesaw seats are like swings: one child per seat. A child who is too light to seesaw with a partner should find a different partner — not add another child to his or her side of the seesaw. Kids should always sit facing one another, not turned around. Teach kids to hold on tightly with both hands while on a seesaw, not to touch the ground or push off with their hands, and to keep feet to the sides, out from underneath the seesaw. Kids should stand back from a seesaw when it's in use. They should never stand beneath a raised seesaw, stand and rock in the middle, or try to climb onto it while it's in motion. ");
+    if(seesawShow == true){
+      seesawShow = false;
+    }else{
+      seesawShow = true;
+    }
   }
 
   chessVid.hide();
-
+  slideVid.hide();
 
 } //end of setup
 
@@ -208,42 +220,56 @@ function draw() {
   image(parkImage, 20,0, parkImage.width/2, parkImage.height/2);
   image(signImage, 410,460, signImage.width/4, signImage.height/4);
   image(chessImage, 720,420, chessImage.width/10, chessImage.height/10);
+  image(ropeImage, 580,220, ropeImage.width/4, ropeImage.height/4);
+  image(seesawImage, 320,330, seesawImage.width/5, seesawImage.height/5);
+
+  if(mouseX > birdX && mouseX < birdX+birdW && mouseY > birdY && mouseY < birdY+birdH){
+    console.log("In rectangle");
+    text("Birds, also known as Aves or avian dinosaurs, are a group of endothermic vertebrates, characterised by feathers, toothless beaked jaws, the laying of hard-shelled eggs, a high metabolic rate, a four-chambered heart, and a strong yet lightweight skeleton.", 30,10,150,200);
+  }
+  fill("black");
+  text("This is a BIRD!", birdX-5, birdY-15);
+  fill("white");
+  rect(birdX,birdY,birdW,birdH);
+
+  image(birdImage, 220,90, birdImage.width/6, birdImage.height/6);
+
+  stretchy.velocity.x = (mouseX-stretchy.position.x)/10;
+  stretchy.velocity.y = (mouseY-stretchy.position.y)/10;
+  drawSprites();
+
+  if(swingShow == true){
+    rect(15,40,145,300);
+    text("Kids should always sit in the swing, not stand or kneel. They should hold on tightly with both hands while swinging, and when finished swinging, stop the swing completely before getting off. Children should stay a safe distance from other kids on swings, being careful not to run or walk in front of or in back of moving swings. Kids should never ride with more than one child to a swing. Swings are designed to safely hold only one person. ", 20,50,140,305);
+  }
+
+  if(slideShow == true){
+    rect(260,8,280,77);
+    text("Kids should always slide down feet first and sitting up, never head first or on their back or stomach. Only one child should be on the slide platform at a time, and kids shouldn't slide down in groups. Kids should always check that the bottom of the slide is clear before sliding down. ", 265,12,275,80);
+    slideVid.show();
+    slideVid.play();
+    slideVid.size(220,230);
+    slideVid.position(640,35);
+  }
+
   if(chessShow == true){
     rect(790,420,205,165);
     text("The player controlling the white pieces is named 'White'; the player controlling the black pieces is named 'Black'. White moves first, then players alternate moves. Making a move is required; it is not legal to skip a move, even when having to move is detrimental. Play continues until a king is checkmated, a player resigns, or a draw is declared. ", 795,430,200,170);
     chessVid.show();
     chessVid.play();
     chessVid.size(240,230);
-    chessVid.position(790,300);
+    chessVid.position(850,300);
   }
 
-  image(ropeImage, 580,220, ropeImage.width/4, ropeImage.height/4);
   if(ropeShow == true){
-    rect(490,360,165,165);
-    text("In speed events, a jumper alternates their feet with the rope going around the jumper every time one of their feet hit the ground for 30 seconds, one minute, or three minutes. The jumper is judged on the number of times the right foot touches the ground in those times. ", 495,370,160,170);
-    rect(750,10,230,360);
-    fill(0);
-    textAlign(CENTER);
-    text('Press X to jump!', width/2, 20);
-    asterisk.velocity.y += GRAVITY;
-    if(asterisk.collide(platform)) {
-      asterisk.velocity.y = 0;
-      asterisk.changeAnimation('normal');
-    }
-    if(keyWentDown('x') || mouseWentDown(LEFT))
-    {
-      asterisk.changeAnimation('stretch');
-      asterisk.animation.rewind();
-      asterisk.velocity.y = -JUMP;
-    }
+    rect(580,375,135,205);
+    text("In speed events, a jumper alternates their feet with the rope going around the jumper every time one of their feet hit the ground for 30 seconds, one minute, or three minutes. The jumper is judged on the number of times the right foot touches the ground in those times. ", 585,385,130,190);
   }
 
-  image(seesawImage, 320,330, seesawImage.width/5, seesawImage.height/5);
-  image(birdImage, 220,90, birdImage.width/6, birdImage.height/6);
-
-  stretchy.velocity.x = (mouseX-stretchy.position.x)/10;
-  stretchy.velocity.y = (mouseY-stretchy.position.y)/10;
-  drawSprites();
+  if(seesawShow == true){
+    rect(185,150,200,210);
+    text("Seesaw seats are like swings: one child per seat. A child who is too light to seesaw with a partner should find a different partner — not add another child to his or her side of the seesaw. Kids should always sit facing one another, not turned around. Teach kids to hold on tightly with both hands while on a seesaw, not to touch the ground or push off with their hands, and to keep feet to the sides, out from underneath the seesaw. ", 190,160,195,190);
+  }
 
   signClick.draw();
   swingClick.draw();
